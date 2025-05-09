9d41213ed2693057a456588234b9df8ef743899f
MinenhleSolson <minenhlecele34@gmail.com>
2025-05-09 12:45:53 +0200

    fourth commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,14 @@ const eslintConfig = [
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-explicit-any": "off",
       "prefer-const": "off",
+      "@typescript-eslint/ban-ts-comment": "off",             // Allow @ts-ignore and others
+      "@typescript-eslint/ban-types": "off",                  // Allow use of `Function`, `object`, etc.
+      "@typescript-eslint/consistent-type-assertions": "off", // Loosen type assertion rules
+      "@typescript-eslint/explicit-module-boundary-types": "off", // Allow implicit return types
+      "@typescript-eslint/no-unsafe-assignment": "off",       // Allow unsafe values to be assigned
+      "@typescript-eslint/no-unsafe-member-access": "off",    // Allow accessing properties on unknown
+      "@typescript-eslint/no-unsafe-call": "off",             // Allow calling possibly unsafe values
+      "@typescript-eslint/no-unsafe-argument": "off",         // Allow unsafe function args
     },
   },
 ];
